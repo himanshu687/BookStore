5a6ad75bc19479875ea4c0a27044161de761a25a
Himanshu <himanshuhisar687@gmail.com>
2022-07-03 20:07:47 +0530

    Added github action file, Added heroku deployment, Added settings.properties file

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.weCode.bookStore.controller;
+
+import com.weCode.bookStore.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks() {
+        BookDto book = BookDto.builder()
+                .title("My First Book Title.")
+                .build();
+
+        BookDto bookSecond = BookDto.builder()
+                .title("Second Book Title.")
+                .build();
+
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+        books.add(bookSecond);
+        return ResponseEntity.ok(books);
+    }
+}
+
